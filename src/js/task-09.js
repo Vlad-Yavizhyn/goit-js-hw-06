15c3bf01953a649d14c50db047bc10734a748fc3
Vladlen <91382235+Vlad-Yavizhyn@users.noreply.github.com>
2022-04-25 16:18:00 +0300

    task-9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
-function getRandomHexColor() {
+const getRandomHexColor = () => {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const refs = {
+  btnChangeColor: document.querySelector('.change-color'),
+  textColor: document.querySelector('.color'),
+}
+
+function onClickBtn() {
+  refs.textColor.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+refs.btnChangeColor.addEventListener('click', onClickBtn);
