68d72061ffee8651d314bc232be3081d2c6301be
Vladlen <91382235+Vlad-Yavizhyn@users.noreply.github.com>
2022-04-25 15:58:02 +0300

    task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) { 
+    event.preventDefault()
+
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+  const password = formElements.password.value;
+  
+  if (mail === '' || password === '') { 
+        alert('Всі поля мають бути заповнені!')
+  }
+  
+    const formData = {
+        mail,
+        password
+    }
+    console.log(formData)
+    form.reset();
+};
